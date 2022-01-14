90eb35b27618ce8060912acb1f0c2f0f1ff2afc8
MikeeI <MikeeI@users.noreply.github.com>
2022-01-14 20:06:49 +0100

    Create feed.php

---
+++ feed.php
@@ -0,0 +1,24 @@
+<?php
+    $feed = new RSS2( );
+
+    $channel = $feed->addChannel( );
+
+    $channel
+        ->title( 'My Blog' )
+        ->description( 'My personal blog' )
+        ->link( 'https://example.com' )
+        ->lastBuildDate( new \DateTime( ) )
+        ->pubDate( new \DateTime( ) )
+        ->language( 'en-US' );
+
+    foreach( $posts as $post ) {
+        $channel->addItem( )
+            ->title( $post->title )
+            ->description( $post->description )
+            ->link( $post->url )
+            ->pubDate( $post->publishedAt )
+            ->guid( $post->url, true );
+    }
+
+    print $feed;
+?>
