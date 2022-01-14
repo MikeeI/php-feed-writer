3b3aadbd8785a972523ba378de27aae440121bfd
MikeeI <MikeeI@users.noreply.github.com>
2022-01-14 20:13:44 +0100

    Update feed.php

--- feed.php
+++ feed.php
@@ -3,27 +3,45 @@
     require 'vendor/autoload.php';
 
 
-    use Lukaswhite\FeedWriter\RSS2;
-    $feed = new RSS2( );
+    use Lukaswhite\FeedWriter\Itunes;
+    $feed = new Itunes( );
 
     $channel = $feed->addChannel( );
 
-    $channel
-        ->title( 'My Blog' )
-        ->description( 'My personal blog' )
-        ->link( 'https://example.com' )
-        ->lastBuildDate( new \DateTime( ) )
-        ->pubDate( new \DateTime( ) )
-        ->language( 'en-US' );
+    $channel->title( 'All About Everything' )
+        ->subtitle( 'A show about everything' )
+        ->description( 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our podcast in the Podcasts app or in the iTunes Store' )
+        ->summary( 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our podcast in the Podcasts app or in the iTunes Store' )
+        ->link( 'http://www.example.com/podcasts/everything/index.html' )
+        ->image( 'http://example.com/podcasts/everything/AllAboutEverything.jpg' )
+        ->author( 'John Doe' )
+        ->owner( 'John Doe', 'john.doe@example.com' )
+        ->explicit( 'no' )
+        ->copyright( '&#x2117; &amp; &#xA9; 2014 John Doe &amp; Family' )
+        ->generator( 'Feed Writer' )
+        ->ttl( 60 )
+        ->lastBuildDate( new \DateTime( '2016-03-10 02:00' ) );
 
-    foreach( $posts as $post ) {
-        $channel->addItem( )
-            ->title( $post->title )
-            ->description( $post->description )
-            ->link( $post->url )
-            ->pubDate( $post->publishedAt )
-            ->guid( $post->url, true );
-    }
+    $channel->addCategory()
+        ->term('Technology');
+
+    $channel->addCategory()
+        ->term('Sports')
+        ->children('Football', 'Soccer');
+
+    $channel->addItem( )
+        ->title( 'Shake Shake Shake Your Spices' )
+        ->author( 'John Doe' )
+        ->subtitle( 'A short primer on table spices' )
+        ->duration( '07:04' )
+        ->summary( 'This week we talk about <a href="https://itunes/apple.com/us/book/antique-trader-salt-pepper/id429691295?mt=11">salt and pepper shakers</a>, comparing and contrasting pour rates, construction materials, and overall aesthetics. Come and join the party!' )
+        ->pubDate( new \DateTime( '2016-03-08 12:00' ) )
+        ->guid( 'http://example.com/podcasts/archive/aae20140615.m4a' )
+        ->explicit( 'no' )
+        ->addEnclosure( )
+            ->url( 'http://example.com/podcasts/everything/AllAboutEverythingEpisode3.m4a' )
+            ->length( 8727310 )
+            ->type( 'audio/x-m4a' );
 
     print $feed;
 ?>
