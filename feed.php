37e380d8ddbe1ec86668c1bc4361d040aa9f6b21
MikeeI <MikeeI@users.noreply.github.com>
2022-01-14 20:46:12 +0100

    Create feed.php

--- feed.php
+++ feed.php
@@ -19,7 +19,7 @@
 
     $json_decoded = json_decode($json);
     foreach($json_decoded as $element){
-        echo $element->id;
+        echo $element->items;
     }
     //var_dump($json_decoded);
 
