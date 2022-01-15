dbd95dfb5eb1146bc81f510f422207387748e5c1
MikeeI <MikeeI@users.noreply.github.com>
2022-01-15 20:37:17 +0100

    Update feed.php

--- feed.php
+++ feed.php
@@ -6,7 +6,7 @@
     $BEARER = getenv("BEARER");
     $spotify_show_id="4rOoJ6Egrf8K2IrywzwOMk";
 
-    $ch = curl_init('https://api.spotify.com/v1/shows/' . $spotify_show_id . '/episodes?limit=3');
+    $ch = curl_init('https://api.spotify.com/v1/shows/' . $spotify_show_id . '/episodes?limit=50');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
        'Accept: application/json',
