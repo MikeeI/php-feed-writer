3014465e987174af4380e2fe7e48c71ee2fdec7f
MikeeI <MikeeI@users.noreply.github.com>
2022-01-20 15:38:35 +0100

    Update feed.php

--- feed.php
+++ feed.php
@@ -23,6 +23,8 @@
         $temp_episodes = getEpisodes($spotify_show_id , $limit , $loop_count * $limit );
         echo "temp_episodes: " . gettype($temp_episodes). "\n";
         echo "json_show_episodes: " . gettype($json_show_episodes). "\n";
+        
+        var_dump($temp_episodes[$j]);
 
         for($j = 0; $j < count($temp_episodes); $j++)
         {
